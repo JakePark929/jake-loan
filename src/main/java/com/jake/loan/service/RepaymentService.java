c1d1f6d4f8fff401a5f2160872e073ecf1570756
epike <epikeran@gmail.com>
2023-04-20 10:32:59 +0900

    feat: add code to delete repayment

--- src/main/java/com/jake/loan/service/RepaymentService.java
+++ src/main/java/com/jake/loan/service/RepaymentService.java
@@ -8,4 +8,5 @@ public interface RepaymentService {
     List<RepaymentDTO.ListResponse> get(Long applicationId);
     RepaymentDTO.Response create(Long applicationId, RepaymentDTO.Request request);
     RepaymentDTO.UpdateResponse update(Long repaymentId, RepaymentDTO.Request request);
+    void delete(Long repaymentId);
 }
